9140eab31b8c8e63f3b2c3f76a7f83036b25da22
Hendrik Siemund <hendrik.smd@proton.me>
2024-09-14 23:36:24 +0200

    init

---
+++ include/cmpct.hpp
@@ -0,0 +1,18 @@
+#ifndef CMPCT_CMPCT_HPP
+#define CMPCT_CMPCT_HPP
+
+#include <string>
+
+namespace cmpct {
+
+    struct Cmpct {
+    public:
+        Cmpct(std::string s);
+
+    private:
+        std::string cmpct_;
+    };
+
+}
+
+#endif //CMPCT_CMPCT_HPP
---
+++ src/cmpct.cpp
@@ -0,0 +1,7 @@
+#include "cmpct.hpp"
+
+namespace cmpct {
+
+    Cmpct::Cmpct(std::string s) : cmpct_(s) {};
+
+}
